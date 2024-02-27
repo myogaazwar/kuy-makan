2d5d61b7647a5999a7ab93e0393d3ac92cb7733e
myogaazwar <yogaazwar123@gmail.com>
2024-02-27 11:21:51 +0700

    change output

--- webpack.common.js
+++ webpack.common.js
@@ -12,9 +12,9 @@ module.exports = {
     app: path.resolve(__dirname, 'src/scripts/index.js'),
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
+    path: path.resolve(__dirname, 'build/'),
+    publicPath: '/',
+    filename: 'bundle.js',
   },
   module: {
     rules: [
